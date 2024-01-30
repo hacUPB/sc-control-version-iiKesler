277e0d4651045c09f7a636444d0ab03e22210a70
Kusseler <andresmartinezbedoya@gmail.com>
2024-01-29 22:24:20 -0500

    Las optimizaciones anteriores dañaron el código, entonces los estoy reversando

--- factorial/fact/fact.c
+++ factorial/fact/fact.c
@@ -10,22 +10,25 @@ long long factorial(int n){
 }
 
 int is_prime(int n){
+    if (n <= 1){
+        return 0;
+    }
     for (int i = 2; i < n; i++){
         if (n % i == 0){
             return 0;
         }
     }
-    return n > 1;
+    return 1;
 }
 
 int main(int argc, char *argv[]) {
+    int number;
 
     if (argc != 2) {
         fprintf(stderr, "Usage: program <number>\n");
         return 1;
-    }
-
-    int number = atoi(argv[1]);
+    } else
+        number = atoi(argv[1]);
 
 
     /*It must be verified that the entered number is greater than or equal to zero*/
