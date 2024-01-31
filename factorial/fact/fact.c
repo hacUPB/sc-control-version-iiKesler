55f7e835a75b67f7a77702b6d0c3f9459e1a6d26
Andrés_Martínez <andres.martinezb@upb.edu.co>
2024-01-31 12:34:54 -0500

    Añadida la función para evaluar si un número es primo o no. Actualizado e README.md acorde

--- factorial/fact/fact.c
+++ factorial/fact/fact.c
@@ -9,6 +9,17 @@ long long factorial (int n){
     return fact;
 }
 
+int is_prime(int n){
+    if (n <= 1){
+        return 0;
+    }
+    for (int i = 2; i < n; i++){
+        if (n % i == 0){
+            return 0;
+        }
+    }
+    return 1;
+}
 
 int main(int argc, char *argv[]) {
     if (argc < 2) {
